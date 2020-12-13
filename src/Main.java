978b26ed6990f8c6a42132cc26f690659bbb0c5d
Bird Egop <sampletext32@bk.ru>
2020-12-13 14:11:52 +0300

    Add basic UI, utils, DB

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,17 @@
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import utils.FXMLHelper;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
         primaryStage.show();
+        primaryStage.setWidth(0);
+        primaryStage.setHeight(0);
+        FXMLHelper.setPrimaryStage(primaryStage);
+        FXMLHelper.loadScreen("LoginScreen");
     }
-
 
     public static void main(String[] args) {
         launch(args);
